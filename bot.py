8cfaa28fc22b4083957466280bda6e0f3ff2354c
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2023-03-29 09:24:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,224 +1,47 @@
-import os, logging, asyncio
-from telethon import Button
-from telethon import TelegramClient, events
-from telethon.tl.types import ChannelParticipantAdmin
-from telethon.tl.types import ChannelParticipantCreator
-from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.errors import UserNotParticipantError
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(name)s - [%(levelname)s] - %(message)s'
-)
-LOGGER = logging.getLogger(__name__)
-
-api_id = int(os.environ.get("APP_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("TOKEN")
-client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
-spam_chats = []
-
-@client.on(events.NewMessage(pattern="^/start$"))
-async def start(event):
-  await event.reply(
-    "__**I'm User Mention Bot**, I can mention almost 10000 members in group I Am An Advance Telegram Bot Made By @Thealphaboz 👻\nClick **/help** for more information__",
-    link_preview=False,
-    buttons=(
-      [
-        Button.url('📣 Channel', 'https://t.me/thealphabotz'),
-        Button.url('📦 Owner/My Father', 't.me/adarsh2626')
-      ]
-    )
-  )
-
-@client.on(events.NewMessage(pattern="^/help$"))
-async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
-  await event.reply(
-    helptext,
-    link_preview=False,
-    buttons=(
-      [
-        Button.url('📣 Channel', 'https://TheAlphaBotz'),
-        Button.url('📦 My Father', 't.me/adarsh2626')
-      ]
-    )
-  )
-  
-@client.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
-async def mentionall(event):
-  chat_id = event.chat_id
-  if event.is_private:
-    return await event.respond("__This command can be use in groups and channels!__")
-  
-  is_admin = False
-  try:
-    partici_ = await client(GetParticipantRequest(
-      event.chat_id,
-      event.sender_id
-    ))
-  except UserNotParticipantError:
-    is_admin = False
-  else:
-    if (
-      isinstance(
-        partici_.participant,
-        (
-          ChannelParticipantAdmin,
-          ChannelParticipantCreator
-        )
-      )
-    ):
-      is_admin = True
-  if not is_admin:
-    return await event.respond("__Only admins can mention all!__")
-  
-  if event.pattern_match.group(1) and event.is_reply:
-    return await event.respond("__Give me one argument!__")
-  elif event.pattern_match.group(1):
-    mode = "text_on_cmd"
-    msg = event.pattern_match.group(1)
-  elif event.is_reply:
-    mode = "text_on_reply"
-    msg = await event.get_reply_message()
-    if msg == None:
-        return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
-  else:
-    return await event.respond("__Reply to a message or give me some text to mention others!__")
-  
-  spam_chats.append(chat_id)
-  usrnum = 0
-  usrtxt = ''
-  async for usr in client.iter_participants(chat_id):
-    if not chat_id in spam_chats:
-      break
-    usrnum += 1
-    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-    if usrnum == 5:
-      if mode == "text_on_cmd":
-        txt = f"{usrtxt}\n\n{msg}"
-        await client.send_message(chat_id, txt)
-      elif mode == "text_on_reply":
-        await msg.reply(usrtxt)
-      await asyncio.sleep(2)
-      usrnum = 0
-      usrtxt = ''
-  try:
-    spam_chats.remove(chat_id)
-  except:
-    pass
-
-@client.on(events.NewMessage(pattern="^/cancel$"))
-async def cancel_spam(event):
-  if not event.chat_id in spam_chats:
-    return await event.respond('__There is no proccess on going...__')
-  else:
-    try:
-      spam_chats.remove(event.chat_id)
-    except:
-      pass
-    return await event.respond('__Stopped.__')
-
-print(">> BOT STARTED <<")
-client.run_until_disconnected()
-
-
-
-
-
-import asyncio
-import datetime
 import os
-import random
-import string
-import time
-import traceback
+import logging
 
-import aiofiles
-from pyrogram.errors import (
-    FloodWait,
-    InputUserDeactivated,
-    PeerIdInvalid,
-    UserIsBlocked,
-)
+from telethon import TelegramClient, events
+from pymongo import MongoClient
 
-import config
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-broadcast_ids = {}
+# Set up database connection
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    raise ValueError('No MongoDB URL found in environment variables')
+client = MongoClient(MONGO_URL)
+db = client.get_default_database()
 
-BROADCAST_AS_COPY = config.BROADCAST_AS_COPY
+# Set up Telegram client
+API_ID = os.environ.get('TELEGRAM_API_ID')
+API_HASH = os.environ.get('TELEGRAM_API_HASH')
+BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+if not all([API_ID, API_HASH, BOT_TOKEN]):
+    raise ValueError('Missing Telegram API credentials or bot token')
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
+# Define message handler
+@bot.on(events.NewMessage(pattern='(?i)@my_bot'))
+async def handle_mention(event):
+    user_id = event.message.from_id.user_id
+    chat_id = event.message.chat_id
+    message_id = event.message.id
+    message_text = event.message.text
 
-async def send_msg(user_id, message):
-    try:
-        if BROADCAST_AS_COPY is False:
-            await message.forward(chat_id=user_id)
-        elif BROADCAST_AS_COPY is True:
-            await message.copy(chat_id=user_id)
-        return 200, None
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return send_msg(user_id, message)
-    except InputUserDeactivated:
-        return 400, f"{user_id} : deactivated\n"
-    except UserIsBlocked:
-        return 400, f"{user_id} : blocked the bot\n"
-    except PeerIdInvalid:
-        return 400, f"{user_id} : user id invalid\n"
-    except Exception:
-        return 500, f"{user_id} : {traceback.format_exc()}\n"
-
-
-async def broadcast(m, db):
-    all_users = await db.get_all_notif_user()
-    broadcast_msg = m.reply_to_message
-    while True:
-        broadcast_id = "".join([random.choice(string.ascii_letters) for i in range(3)])
-        if not broadcast_ids.get(broadcast_id):
-            break
-    out = await m.reply_text(
-        text=f"Broadcast Started! You will be notified with log file when all the users are notified."
-    )
-    start_time = time.time()
-    total_users = await db.total_users_count()
-    done = 0
-    failed = 0
-    success = 0
-    broadcast_ids[broadcast_id] = dict(
-        total=total_users, current=done, failed=failed, success=success
-    )
-    async with aiofiles.open("broadcast.txt", "w") as broadcast_log_file:
-        async for user in all_users:
-            sts, msg = await send_msg(user_id=int(user["id"]), message=broadcast_msg)
-            if msg is not None:
-                await broadcast_log_file.write(msg)
-            if sts == 200:
-                success += 1
-            else:
-                failed += 1
-            if sts == 400:
-                await db.delete_user(user["id"])
-            done += 1
-            if broadcast_ids.get(broadcast_id) is None:
-                break
-            else:
-                broadcast_ids[broadcast_id].update(
-                    dict(current=done, failed=failed, success=success)
-                )
-    if broadcast_ids.get(broadcast_id):
-        broadcast_ids.pop(broadcast_id)
-    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await asyncio.sleep(3)
-    await out.delete()
-    if failed == 0:
-        await m.reply_text(
-            text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True,
-        )
+    # Check if user has already been mentioned
+    if db.mentions.find_one({'user_id': user_id, 'chat_id': chat_id}):
+        await bot.send_message(chat_id, f'You already mentioned @{event.message.from_id.username} in this chat!')
     else:
-        await m.reply_document(
-            document="broadcast.txt",
-            caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
-            quote=True,
-        )
-    os.remove("broadcast.txt")
+        # Save mention to database
+        db.mentions.insert_one({'user_id': user_id, 'chat_id': chat_id, 'message_id': message_id, 'message_text': message_text})
+
+        # Respond to user mention
+        response = f'Thanks for mentioning me, @{event.message.from_id.username}!'
+        await bot.send_message(chat_id, response)
+
+# Start bot
+bot.run_until_disconnected()
